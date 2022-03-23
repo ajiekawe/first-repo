8870df71a6aecacfa416bfaad682e25183cc2401
ajiekw <jiekawe@gmail.com>
2022-03-23 12:54:21 +0700

    first commit

---
+++ coba.py
@@ -0,0 +1,21 @@
+a = 'ini apa1'
+b = "ini apa?"
+c = 3
+d = "buku"
+
+#print(a * 3)
+#print("-" * 10)
+#print(a[2:5])
+#print(a.upper())
+#print(a.lower())
+
+#print(a)
+#print('ini apa')
+#print(type(b))
+#print(type(a))
+#print(len(a))
+
+#print(a[0])
+#print(b[7])
+
+#print(a + "," + b)
